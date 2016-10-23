030d38c196eb102d5bda0cb946ea3964f3a5c73a
Joey Guerra <joey_guerra@homedepot.com>
2016-10-23 09:24:46 -0500

    initial commit

---
+++ src/main/java/Money.java
@@ -0,0 +1,38 @@
+package main.java;
+
+public class Money implements Expression {
+	protected int amount;
+	protected String currency;
+	public Money(int amount, String currency){
+		this.amount = amount;
+		this.currency = currency;
+	}
+	public static Money dollar(int amount){
+		return new Money(amount, "USD");
+	}
+	public static Money franc(int amount){
+		return new Money(amount, "CHF");
+	}
+	public Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
+	public Expression plus(Money addend){
+		return new Sum(this, addend);
+	}
+	public boolean equals(Object object){
+		Money money = (Money) object;
+		return amount == money.amount
+				&& currency().equals(money.currency());
+	}
+	public String currency() {
+		return currency;
+	}
+	public String toString(){
+		return amount + " " + currency;
+	}
+	@Override
+	public Money reduce(Bank bank, String to) {
+		int rate = bank.rate(currency, to);
+		return new Money(amount/rate, to);
+	}
+}
---
+++ src/main/java/Sum.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Sum implements Expression{
+	public Money augend;
+	public Money addend;
+	public Sum(Money augend, Money addend){
+		this.augend = augend;
+		this.addend = addend;
+	}
+	public Money reduce(Bank bank, String to){
+		int amount = augend.amount + addend.amount;
+		return new Money(amount, to);
+	}
+}
